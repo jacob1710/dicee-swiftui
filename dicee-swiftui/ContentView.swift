276fab3c890d8ffe9cb8742c4a38219b80e5969e
Jacob Scase <jacobscase@hotmail.com>
2020-11-25 16:06:46 +0000

    Initial Commit

---
+++ dicee-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dicee-swiftui
+//
+//  Created by Jacob Scase on 25/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dicee-swiftui/dicee_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicee_swiftuiApp.swift
+//  dicee-swiftui
+//
+//  Created by Jacob Scase on 25/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct dicee_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
